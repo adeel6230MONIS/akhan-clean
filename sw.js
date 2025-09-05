3c3642293c55b1577e39613fcd20eefb3c54f5c1
adeel6230MONIS <vinayloveraja@gmail.com>
2025-09-05 07:14:37 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'akhan-pwa-v1';
+const urlsToCache = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
